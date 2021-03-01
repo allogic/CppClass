2a0da41d3338d2dba20b6a9f1f53751137505d6c
0x616c <mial.burg@gmail.com>
2021-03-01 04:52:22 -0800

    Argend and initial project environment

---
+++ Agenda/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* Agenda:
+*  - Diff. between .h and .cpp
+*  - What is a translation unit?
+*  - Primitive data types and arrays
+*  - C strings and printf()
+*  - Stack vs. Heap
+*  - Pointer arithmetic and type casting
+*  - Standard C++11/14/17/20/23 library
+*  - C OOP vs. C++ OOP
+*  - V-Tables
+*  - Operator overloading
+*  - Meta programming and templates
+*  - Static vs. Dynamic libraries
+*  - ABI caveats
+*  - Important C/C++ keywords: typedef/static/extern/volatile/inline/__declspec
+*  - Debugger vs. Debuggee
+*  - Write example program!
+*/
+
+int main(int argc, char* argv[], char* envp[])
+{
+  return 0;
+}
